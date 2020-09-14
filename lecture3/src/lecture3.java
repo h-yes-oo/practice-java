7f265fe728ea719db9ee01230f8efc3e9e1c0c33
h-yes-oo <hyesoo5115@naver.com>
2020-09-14 16:11:24 +0900

    lecture3

--- lecture3/src/lecture3.java
+++ lecture3/src/lecture3.java
@@ -21,5 +21,29 @@ public class lecture3 {
         String[] thoughts = {"Coco","is","So","Cute"};
         System.out.println(mythoughts[0]);
         System.out.println(thoughts[0]);
+
+        // 2 X 3
+        int[][] exampleArray = new int[][] {{1,2,3},{4,5,6}};
+
+        // 아래 조건을 만족하는 2-dimensional array 만들기
+        // 1st row ==> 2 items
+        // 2nd row ==> 3 items
+        int[][] exArray = new int[2][];
+        exArray[0] = new int[2];
+        exArray[1] = new int[3];
+
+        for(int i = 0; i < exArray.length; i++){
+            for(int j =0; j < exArray[i].length; j++){
+                System.out.print(exArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //System.out.println(exArray[0][2]); // Overflow 일어남
+
+        int x1 = 1;
+        if(x1 == 1){
+            System.out.println("Coco so cute!");
+        }
     }
 }
